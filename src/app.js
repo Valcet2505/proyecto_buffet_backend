e13c7f440574df5d59cfce3e9ea303438e72c015
Valcet2505 <fernandobonifas25@gmail.com>
2025-07-14 03:08:45 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ const app = express();
 // Middleware
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://proyecto-buffet-backend.onrender.com'
+  'https://proyecto-buffet-backend.onrender.com',
+  'https://proyecto-buffet-frontend.vercel.app'
 ];
 
 app.use(cors({
